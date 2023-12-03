ff2997e16f582f0cdf2be5a84230219aecbf1b76
FiNeX96 <rodrigoaguiar96@ua.pt>
2023-12-03 00:14:59 +0000

    update exercise guide

--- Exercises/More/api.go
+++ Exercises/More/api.go
@@ -13,8 +13,7 @@ import (
 /*
 
 In this exercise, we are going to build a simple API using the GIN Framework and a sqlite database.
-To get the GIN framework, run the following command in your terminal: go get github.com/gin-gonic/gin 
-To get the sqlite driver, run the following command in your terminal: go get github.com/mattn/go-sqlite3
+To get the GIN framework and sqlite driver, run the following command : go mod tidy
 Analyse the code below and try to understand what is happening.
 After that, complete the exercises below.
 
@@ -23,7 +22,7 @@ If you are struggling, you may find these guides helpful:
 - https://go.dev/doc/tutorial/web-service-gin
 - https://zetcode.com/golang/sqlite3/
 
-Can always ask for help aswell =)
+Can always ask for help as well =)
 
 */
 
@@ -31,13 +30,23 @@ func main (){
 
 	router := gin.Default()
 
-	// Basic Endpoint creation, you can access it by going to http://localhost:8080/hello
+
+
+	/* 
+
+	We are creating a basic handler for /hello endpoint.
+	When a request is made to this endpoint, it calls a function that receives a gin.Context pointer.
+	This pointer contains information about the request made such as parameters, headers, etc.
+	We then use c.JSON to return a JSON object with the message "Hello World!" ( this is basically the 'return' of the function, no need for it )
+
+	*/
 
 	router.GET("/hello", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello World!",
 		})
 	})
+
 
 	// Lets turn this API into a web server to manage books for a library 
 
@@ -46,7 +55,7 @@ func main (){
 	// 1. Start by creating a struct to represent a book. It should have the following fields: ID, Title, Artist, Price
 
 
-		/* 2 . Implement a JSON serialization method in your struct, you can do this as follows 
+	/* 2 . Implement a JSON serialization method in your struct, you can do this as follows 
 
 		type Book struct {
 			ID int `json:"id"`
@@ -91,7 +100,7 @@ func main (){
 	*/
 
 
-	// 5 . Now that we have books in the databse, create a endpoint to get all the books in the database using the GET method.
+	// 5 . Now that we have books in the database, create a endpoint to get all the books in the database using the GET method.
 
 
 	// 6. Develop a endpoint to get a book by its ID using the GET method.
